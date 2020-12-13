ef93cce030911baf85144540e021433856b520b8
Sujith <sujithchandrasekaran@gmail.com>
2020-12-13 22:07:08 +0530

    Login button added

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
-import { Card,  CardContent, Typography,  FormControl,  Input, InputLabel } from '@material-ui/core';
+import { Card,  CardContent, Typography,  FormControl,  Input, InputLabel, Button } from '@material-ui/core';
 import './Login.css';
 
 export default class Login extends Component{
+
+
+    loginhandler () {
+        alert ("login button");
+    }
+
     render() {
         return (
             <div>
                 <Card className="login-card" >
                     <CardContent>
-                        <FormControl margin="normal" size="medium" variant="standard">
+                        <FormControl margin="normal" size="big" variant="standard">
                             <Typography variant="h5" component="h5" color="textPrimary"
                             >LOGIN</Typography>
                         </FormControl>
                         <FormControl fullWidth required margin="normal" size="medium" variant="standard">
-                            <InputLabel htmlFor="ip-uname">Username</InputLabel>
-                            <Input id="ip-uname" type="text" />
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input type="text" />
                         </FormControl>
                         <FormControl fullWidth required margin="normal" size="medium" variant="standard">
-                            <InputLabel htmlFor="ip-passwd">Password</InputLabel>
-                            <Input id="ip-passwd" type="password"/>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input type="password"/>
 
+                        </FormControl>
+
+                        <FormControl margin="normal" size="medium" variant="standard">
+                            <Button variant="contained" color="primary" onClick={this.loginhandler}>LOGIN</Button>
                         </FormControl>
                         
                     </CardContent>
